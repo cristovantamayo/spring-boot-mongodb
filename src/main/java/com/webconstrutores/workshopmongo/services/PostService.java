dd7736f624d99ee6962cff5253e40d2495493e06
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-26 08:30:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/webconstrutores/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.webconstrutores.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webconstrutores.workshopmongo.domain.Post;
+import com.webconstrutores.workshopmongo.repository.PostRepository;
+import com.webconstrutores.workshopmongo.services.exception.ObjecctNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjecctNotFoundException("Usuário não encontrado"));
+	}
+}
