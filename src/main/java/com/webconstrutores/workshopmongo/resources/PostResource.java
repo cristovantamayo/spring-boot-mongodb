4419ed4e5249b4ee5788b30541ca24c01321a5f4
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-26 09:04:20 -0300

    Consulta simples com query methods

--- src/main/java/com/webconstrutores/workshopmongo/resources/PostResource.java
+++ src/main/java/com/webconstrutores/workshopmongo/resources/PostResource.java
@@ -1,21 +1,17 @@
 package com.webconstrutores.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webconstrutores.workshopmongo.domain.Post;
-import com.webconstrutores.workshopmongo.domain.User;
-import com.webconstrutores.workshopmongo.dto.UserDTO;
+import com.webconstrutores.workshopmongo.resources.util.URL;
 import com.webconstrutores.workshopmongo.services.PostService;
 
 @RestController
@@ -29,6 +25,12 @@ public class PostResource {
 	private ResponseEntity<Post> findById( @PathVariable String id) {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	private ResponseEntity<List<Post>> findByTitle( @RequestParam(value="text", defaultValue = "") String text) {
+		List<Post> posts = service.fincByTitle(URL.decodeParam(text));
+		return ResponseEntity.ok().body(posts);
 	}
 	
 }
--- src/main/java/com/webconstrutores/workshopmongo/services/PostService.java
+++ src/main/java/com/webconstrutores/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.webconstrutores.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new ObjecctNotFoundException("Usuário não encontrado"));
+	}
+	
+	public List<Post> fincByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
